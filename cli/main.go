96daaed5dd4283e69e0a57953334637c357129a8
yafet-a <alulayafet@gmail.com>
2025-01-12 18:11:17 +0000

    refactor: returning actual file data rather than directly printing

--- cli/main.go
+++ cli/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"cli/commands"
+	"cli/tui"
+	"dltfm/pkg/models"
 	"fmt"
 	"os"
 )
@@ -14,6 +16,9 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
+	case "tui":
+		// Start the TUI
+		tui.Start()
 	case "register":
 		if len(os.Args) < 3 {
 			fmt.Println("Usage: dltfm register <filepath>")
@@ -27,8 +32,11 @@ func main() {
 			fmt.Println("File successfully registered")
 		}
 	case "queryall":
-		if err := commands.QueryAllFiles(); err != nil {
+		files, err := commands.QueryAllFiles()
+		if err != nil {
 			fmt.Printf("Error querying files: %v\n", err)
+		} else {
+			fmt.Println(models.FormatFileList(files))
 		}
 	case "query":
 		if len(os.Args) < 3 {
@@ -36,8 +44,11 @@ func main() {
 			return
 		}
 		fileID := os.Args[2]
-		if err := commands.QueryFileByID(fileID); err != nil {
+		file, err := commands.QueryFileByID(fileID)
+		if err != nil {
 			fmt.Printf("Error querying file: %v\n", err)
+		} else {
+			fmt.Println(models.FormatFileList([]models.File{*file}))
 		}
 	default:
 		fmt.Println("Unknown command")
