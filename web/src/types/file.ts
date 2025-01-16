85e748725a7e03ecb9fb72202da807f7a336626d
yafet-a <alulayafet@gmail.com>
2025-01-16 16:21:32 +0000

    feature: simple frontend fetching and displaying files from the ledger

---
+++ web/src/types/file.ts
@@ -0,0 +1,11 @@
+export interface File {
+    id: string;
+    name: string;
+    hash: string;
+    storageLocation: string;
+    timestamp: string;
+    owner: string;
+    metadata: string;
+    version: number;
+    previousHash?: string;
+  }
